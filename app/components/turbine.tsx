98f22890ceb28599c7e8c7388823300f39e6bd3f
Prince Asiedu <princekofasiedu@gmail.com>
2025-07-28 18:39:05 +0000

    first commit

---
+++ app/components/turbine.tsx
@@ -0,0 +1,14 @@
+export function Turbine() {
+    return (
+        <div className="turbine">
+            <div className="pilot">
+                <div className="prop-container">
+                    <div className="prop"></div>
+                    <div className="prop"></div>
+                    <div className="prop"></div>
+                </div>
+            </div>
+            <div className="pole"></div>
+        </div>
+    )
+}
